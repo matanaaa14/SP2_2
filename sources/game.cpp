3647a6468279940c32228b4b1a2e4348e5e97b0f
matanaaa14 <117116975+matanaaa14@users.noreply.github.com>
2023-04-11 18:40:14 +0300

    Add files via upload

--- sources/game.cpp
+++ sources/game.cpp
@@ -6,45 +6,41 @@ using namespace std;
 #include <iostream>
 #include <random>
 #include <stdexcept>
-#include<string.h>
+#include <string.h>
 
 using namespace std;
     void Game::playTurn(){
+        if(this->player1.name == this->player2.name){
+            throw runtime_error("can't play against himself");
+        }
         this->numOfRounds+=1;
         if(this->player1.myCards.size() == 0 || this->player2.myCards.size() == 0 ){
              throw logic_error("on of the players already finish all his cards ");
         }
         
-        Card c1=this->player1.myCards.top();
-        Card c2=this->player2.myCards.top();
-        //cout << "the card is: "<< c1.toString() <<endl;
-        //cout << "the second card is: "<< c2.toString() <<endl;
+        Card c1 = this->player1.myCards.top();
+        Card c2 = this->player2.myCards.top();
         player1.myCards.pop();
         player2.myCards.pop();
-        //cout << "card 1 value: " << c1.value <<endl;
-        //cout << "card 2 value: " << c2.value <<endl;
 
         if(c1.value == c2.value){
-            //cout << "-----------------" << endl;
-            //cout << player1.myCards.size() <<endl;//check
-            this->lastTurn= player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type ;
+            this->lastTurn = player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type ;
             this->allTurns.push(this->lastTurn);
 
             while(c1.value == c2.value && player1.myCards.size() >= 2 && player2.myCards.size() >= 2 ){
-                //cout << "$$$$$$$$$$" << endl;
                 cardsOnTable1.push(c1);
                 cardsOnTable2.push(c2);
                 cardsOnTable1.push(player1.myCards.top());
                 cardsOnTable2.push(player2.myCards.top());
                 player1.myCards.pop();
                 player2.myCards.pop();
-                Card c1=this->player1.myCards.top();
-                Card c2=this->player2.myCards.top();
+                Card c1 = this->player1.myCards.top();
+                Card c2 = this->player2.myCards.top();
                 player1.myCards.pop();
                 player2.myCards.pop();
                 cardsOnTable1.push(c1);
                 cardsOnTable2.push(c2);
-                this->lastTurn+= ", " + player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type ;
+                this->lastTurn += ", " + player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type ;
                 this->allTurns.push(this->lastTurn);
             }
             if(this->player1.stacksize() == 0 || this->player2.stacksize() ==0){
@@ -70,7 +66,7 @@ using namespace std;
                 this->player1.takenCards.push(cardsOnTable2.top());
                 cardsOnTable2.pop();
             }
-            this->player1.numOfWins+=1;
+            this->player1.numOfWins += 1;
             return;
         }
         if(c1.value < c2.value){
@@ -82,27 +78,27 @@ using namespace std;
                 this->player2.takenCards.push(cardsOnTable2.top());
                 cardsOnTable2.pop();
             }
-            this->player2.numOfWins+=1;
+            this->player2.numOfWins += 1;
             return;
         }
         }
-        if(this->player1.stacksize() == 0 || this->player2.stacksize() ==0){
+        if(this->player1.stacksize() == 0 || this->player2.stacksize() == 0){
         this->player1.playing = false;
         this->player2.playing = false;} 
         if(c1.value > c2.value){
             player1.takenCards.push(c1);
             player1.takenCards.push(c2);
-            this->lastTurn= player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type + "," + player1.name + " won";
+            this->lastTurn = player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type + "," + player1.name + " won";
             this->allTurns.push(this->lastTurn);
-            this->player1.numOfWins+=1;
+            this->player1.numOfWins += 1;
             return;
         }
         if(c1.value < c2.value){
             player2.takenCards.push(c1);
             player2.takenCards.push(c2);
-            this->lastTurn= player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type + "," + player2.name + " won";
+            this->lastTurn = player1.name + " played " +c1.strValue +" of "+ c1.type + " " + player2.name + " played " +c2.strValue +" of "+ c2.type + "," + player2.name + " won";
             this->allTurns.push(this->lastTurn);
-            this->player2.numOfWins+=1;
+            this->player2.numOfWins += 1;
             return;
 
         }
@@ -125,7 +121,7 @@ using namespace std;
         if(this->player1.cardesTaken() == this->player2.cardesTaken()){
             cout << "there is no winner " << endl;
         }
-        if(this->player1.cardesTaken() > this->player2.cardesTaken()){
+        else if(this->player1.cardesTaken() > this->player2.cardesTaken()){
             cout << "the winner is: " << this->player1.name << endl;
         }
         else{
@@ -149,8 +145,8 @@ using namespace std;
     }
     void Game::printStats(){
         double winRate1, winRate2;
-        winRate1 =100 * (double) this->player1.numOfWins/this->numOfRounds;
-        winRate2 =100 * (double) this->player2.numOfWins/this->numOfRounds;
+        winRate1 = 100 * (double) this->player1.numOfWins/this->numOfRounds;
+        winRate2 = 100 * (double) this->player2.numOfWins/this->numOfRounds;
         cout << player2.numOfWins << endl;
         cout << player1.name + " won " << winRate1 << "% " << "and have " << player1.cardesTaken() << " cards" << endl; 
         cout << player2.name + " won " << winRate2 << "% " << "and have " << player2.cardesTaken() << " cards" << endl; 
@@ -162,14 +158,16 @@ using namespace std;
             cout << "player 1 or 2 already playing and can't join another game" << endl;
             exit(1);
         }
+        /*/if(player1.name == player2.name){
+            cout<<"---------------"<<endl;
+            throw runtime_error("can't play against himself");
+        }/*/
         this->numOfRounds = 0;
         this->lastTurn = "";
         p1.playing = true;
         p2.playing = true;
-        //this->player1 = p1;
-        //this->player2 = p2;
-        for(int i=2;i<=14;i++){
-            for(int j=1;j<=4;j++){
+        for(int i = 2;i <= 14; i++){
+            for(int j=1; j <= 4; j++){
                 string t=cardType(j);
                 Card card(i,t);
                 this->gameCards.push(card);
@@ -182,13 +180,13 @@ using namespace std;
             this->gameCards.pop();
 
         }
-
-        std::shuffle(v.begin(),v.end(),default_random_engine(7));
-        for(unsigned long b=0; b < v.size(); b++){
+        unsigned long randomSeed = (unsigned long) rand() %100;
+        std::shuffle(v.begin(),v.end(),default_random_engine(randomSeed));
+        for(unsigned long b = 0; b < v.size(); b++){
             this->gameCards.push(v[b]);
 
         }
-        int c=0;
+        int c = 0;
 
         while(!this->gameCards.empty()){
             if(c % 2 == 0){
